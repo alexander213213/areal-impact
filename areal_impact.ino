e8f0661636dec4c130ef7de34b578e187a99ec80
Alexander Gracilla <alexandergracilla@gmail.com>
2024-11-13 04:29:58 +0800

    For testing

--- areal_impact.ino
+++ areal_impact.ino
@@ -1,17 +1,19 @@
-#include <SPI.h>
+// #include <SPI.h>
 #include <TFT_eSPI.h>
-#include <XPT2046_Touchscreen.h>
+// #include <XPT2046_Touchscreen.h>
 #include "play.h"
 
-#define XPT2046_IRQ 5   // T_IRQ
-#define XPT2046_MOSI 23  // T_DIN
-#define XPT2046_MISO 18  // T_OUT
-#define XPT2046_CLK 19   // T_CLK
-#define XPT2046_CS 22    // T_CS
+// #define XPT2046_IRQ 5   // T_IRQ
+// #define XPT2046_MOSI 23  // T_DIN
+// #define XPT2046_MISO 18  // T_OUT
+// #define XPT2046_CLK 19   // T_CLK
+// #define XPT2046_CS 22    // T_CS
 
-// Initialize the TFT display and touchscreen
-SPIClass touchscreenSPI = SPIClass(VSPI);
-XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+
+
+// // Initialize the TFT display and touchscreen
+// SPIClass touchscreenSPI = SPIClass(VSPI);
+// XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 320
@@ -23,10 +25,12 @@ uint16_t x, y, z;
 int centerX = SCREEN_WIDTH / 2;
 int centerY = SCREEN_HEIGHT / 2;
 
+TFT_eSPI tft = TFT_eSPI();
+
 void setup() {
-  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-  touchscreen.begin(touchscreenSPI);
-  touchscreen.setRotation(1);
+//   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+//   touchscreen.begin(touchscreenSPI);
+//   touchscreen.setRotation(1);
 
   tft.init();
   tft.setRotation(1);
