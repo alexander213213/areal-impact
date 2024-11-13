5fca62170fc9720852c12d49100a4f6c4e158ddb
Alexander Gracilla <alexandergracilla@gmail.com>
2024-11-13 12:01:16 +0800

    fix

--- areal_impact.ino
+++ areal_impact.ino
@@ -1,26 +1,15 @@
 // #include <SPI.h>
 #include <TFT_eSPI.h>
 // #include <XPT2046_Touchscreen.h>
-#include "play.h"
-
-// #define XPT2046_IRQ 5   // T_IRQ
-// #define XPT2046_MOSI 23  // T_DIN
-// #define XPT2046_MISO 18  // T_OUT
-// #define XPT2046_CLK 19   // T_CLK
-// #define XPT2046_CS 22    // T_CS
-
-
-
-// // Initialize the TFT display and touchscreen
-// SPIClass touchscreenSPI = SPIClass(VSPI);
-// XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 320
 #define FONT_SIZE 3
 
 // Touchscreen coordinates: (x, y) and pressure (z)
-uint16_t x, y, z;
+uint16_t x
+uint16_t y
+uint16_t z;
 
 int centerX = SCREEN_WIDTH / 2;
 int centerY = SCREEN_HEIGHT / 2;
@@ -42,14 +31,14 @@ void setup() {
   tft.setTextColor(TFT_BLACK, TFT_WHITE);
   
   playscreen.createSprite(480, 320);
-  playscreen.setSwapBytes(true)
+  playscreen.setSwapBytes(true);
   playscreen.pushImage(0, 0, 480, 320, play);
-  playscreen.pushSprite(0,0, TFT_WHITE)
+  playscreen.pushSprite(0,0, TFT_WHITE);
   
 }
 
 void loop() {
   tft.getTouchRaw(&x, &y);
-  tft.convertRawXZ(&x, &y)
+  tft.convertRawXY(&x, &y);
     
 }
